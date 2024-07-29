c53ac94f13dfc06b7627949122d20a217bf0b0e8
delano <delano@cpan.org>
2024-07-29 09:41:38 -0700

    Run vite on consistent port, host
    e.g. Equivalent to `vite --port 5177 --host`

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port: 5177,
+    host: true // This is equivalent to the --host option
   }
 })
