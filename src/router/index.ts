79e193b1a5009712ae97dc7bac55e2433d67c735
delano <delano@cpan.org>
2024-07-29 18:37:57 -0700

    Add routes for 404 page and maintenance preview

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Maintenance from '@/views/Maintenance.vue';
+import NotFound from '@/views/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,19 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Maintenance,
+    },
+
+    // For previews
+    {
+      path: '/errors',
+      name: 'errors',
+      component: Maintenance,
+    },
+    // Default route (404 Not Found)
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: NotFound,
     },
   ],
 });
